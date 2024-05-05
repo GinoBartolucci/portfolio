82da4fb56d6436c9d6a661cd401cbe23b2d27964
gino bartolucci <ginobartolucci00@gmail.com>
2024-05-05 18:28:33 -0300

    package, gitignore, deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:GinoBartolucci/portfolio.git master:gh-pages
+git push -f git@github.com:GinoBartolucci/portfolio.git main:gh-pages
 
 cd -
