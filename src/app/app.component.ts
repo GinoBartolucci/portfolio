ac8f04f7cbfc65e882ac68b45a9fce1feb604257
gino bartolucci <ginobartolucci00@gmail.com>
2025-04-02 13:43:00 -0300

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {AboutComponent} from './about/about.component'
 import {ProjectsComponent} from './projects/projects.component'
 import {FooterComponent} from './footer/footer.component'
 import {ContactComponent} from './contact/contact.component'
-import {ExperienciaComponent} from './experiencia/experiencia.component'
+//import {ExperienciaComponent} from './experiencia/experiencia.component'
 
 @Component({
   selector: 'app-root',
